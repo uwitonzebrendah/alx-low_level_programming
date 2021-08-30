67ea888d2b32db5e847bdbb417f7ae31a3cff4cb
brendahuwitonze <brendahuwitonze1@gmail.com>
2021-08-30 00:54:00 -0700

    edited the 4puts code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - Entry point
@@ -10,3 +10,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
+
