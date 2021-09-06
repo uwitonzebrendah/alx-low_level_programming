ac9cd46775cb82e0c6b755cecb23c032778ff3c8
brendah <brendahuwitonze1@gmail.com>
2021-09-07 01:58:07 +0200

    edited file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * rev_string - check the code for Holberton School students.
  * @s: pointer int type
@@ -8,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-int i,j;
+int i, j;
 char temp[500];
 
 for (i = 0; *s != '\0'; i++)
@@ -18,6 +17,7 @@ s++;
 }
 
 s--;
+
 for (j = 0; j != i; j++)
 {
 *s = temp[j];
