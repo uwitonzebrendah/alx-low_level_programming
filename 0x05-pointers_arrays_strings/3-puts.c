8e28783adf17d17725a647a33f3de30d42ffa0f4
brendah <brendahuwitonze1@gmail.com>
2021-09-06 21:48:38 +0200

    no fear

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: pointer int type
+ * Return: Always 0.
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{		
+	_putchar(*str);
+	}	
+}
