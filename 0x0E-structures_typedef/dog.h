8b34dc78d416e528ddcc1595b937a8fd1a730013
uwitonzebrendah <brendahuwitonze1@gmail.com>
2021-09-28 12:35:39 +0200

    betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,10 +9,10 @@
  */
 typedef struct dog
 {
-	char*name;
+	char *name;
 	float age;
-	char*owner;
-}dog_t;
+	char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
