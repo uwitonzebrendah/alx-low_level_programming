75d564808c2cfb5032028d71d9ce057025b7959f
uwitonzebrendah <brendahuwitonze1@gmail.com>
2021-09-28 12:31:14 +0200

    dog identity

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
+
+/**
+ *struct dog -structure definition of dog
+ *@name:dog name
+ *@age:dog age
+ *@owner:dog owner
+ */
+typedef struct dog
+{
+	char*name;
+	float age;
+	char*owner;
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
